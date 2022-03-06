c3edf9cd56cdb9485c4f2c3eb738d949d41cc7a1
Abdul Shaikh <ashaikh@webuy.com>
2022-03-06 20:22:43 +0530

    Basket Project files.

--- src/services/basket/Basket.API/Program.cs
+++ src/services/basket/Basket.API/Program.cs
@@ -1,14 +1,33 @@
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddStackExchangeRedisCache(option=>
-    {
-        option.Configuration = "localhost:6379";
-    }
-);
+// Redis Configuration
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration["CacheSettings:ConnectionString"];    
+});
 
+// General Configuration
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddAutoMapper(typeof(StartupBase));
+
+// Grpc Configuration
+//builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+            //(o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+//builder.Services.AddScoped<DiscountGrpcService>();
+
+// MassTransit-RabbitMQ Configuration
+//builder.Services.AddMassTransit(config => {
+//    config.UsingRabbitMq((ctx, cfg) => {
+//        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+//    });
+//});
+//builder.Services.AddMassTransitHostedService();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
